5cd61e100882e8ad1833aae586b6df2845e09138
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2021-12-23 12:54:19 +0500

    video responsiveness corrected

--- src/components/singleLargeVideoViews/index.jsx
+++ src/components/singleLargeVideoViews/index.jsx
@@ -6,7 +6,7 @@ import styles from "./singleLargeVideoViews.module.css";
 const SingleLargeVideoViews = () => {
   return (
     <div
-      className={`flex items-center justify-center mt-5 ${styles.single_large_video_views}`}
+      className={`flex items-center justify-center mt-5 shadow-xl ${styles.single_large_video_views}`}
     >
       <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
     </div>
