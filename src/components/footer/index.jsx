b188ab441a6e4e3e45a7d084deeb45602b923ee5
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2021-12-23 17:04:44 +0500

    Footer completed

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,59 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>This is Footer</div>;
+  return (
+    <div className="bg-black text-white p-20">
+      <div className="grid grid-cols-1 my-5 lg:grid-cols-3 md:grid-cols-2">
+        <div>
+          <div className="text-2xl font-bold">Submit Project</div>
+          <div>
+            <div>Lorem, ipsum.</div>
+            <div>Lorem, ipsum.</div>
+            <div>Lorem, ipsum.</div>
+          </div>
+        </div>
+        <div>
+          <div className="text-2xl font-bold">Contact</div>
+          <div className="w-11/12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            nisi laudantium blanditiis omnis, quasi similique facilis adipisci
+            ullam ab dicta.Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Aliquam nisi laudantium blanditiis omnis, quasi similique
+            facilis adipisci ullam ab dicta.
+          </div>
+        </div>
+        <div className="ml-5">
+          <div className="text-2xl font-bold">Stay in touch</div>
+          <div className="flex flex-col ">
+            <input type="text" className="rounded-md my-3 text-black p-1" />
+            <button className="bg-gradient-to-r from-blue-200 to-blue-400 rounded-md text-black p-1">
+              Hello World
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="h-[4px] bg-gradient-to-r from-black to-blue-400" />
+      <div className="grid grid-cols-1 my-5 text-right md:grid-cols-2 lg:grid-cols-3">
+        <div>
+          <img
+            src="/assets/Footer-Logo.png"
+            alt="Footer Logo"
+            className="w-48"
+          />
+        </div>
+        <div>
+          <a href="/" className="border-r-2 pr-3 mr-3">
+            Terms & Condition
+          </a>
+          <a href="/" className="border-r-2 pr-3 mr-3">
+            Privacy Policy
+          </a>
+          <a href="/">Disclaimer</a>
+        </div>
+        <div>2021 privacy policy</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/ourpartners/index.jsx
+++ src/components/ourpartners/index.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 
 const OurPartners = () => {
     return (
-        <div className='py-12'>
-            <SectionHeader heading='Our Partners'/>
-            <div className='grid place-items-center h-screen'>
-                <img className='w-1/12' src="/assets/Homepage-Assets/QUESTION-MARK.png" alt="" />
-            </div>
+      <div className="py-12">
+        <SectionHeader heading="Our Partners" />
+        <div className="grid place-items-center h-[50vh]">
+          <img
+            className="w-1/12"
+            src="/assets/Homepage-Assets/QUESTION-MARK.png"
+            alt=""
+          />
         </div>
-    )
+      </div>
+    );
 }
 
 export default OurPartners
