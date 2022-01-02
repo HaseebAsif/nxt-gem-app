8bf5e16e06b37c5e2b1c8e6d144c3d6c18e99a51
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-01-02 18:15:36 +0500

    Server.js file added for bluehost

--- src/components/advertisementTop/index.jsx
+++ src/components/advertisementTop/index.jsx
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
 
-const AdvertisementTop = () => {
-  const [AdvertisementData, setAdvertisementData] = useState({});
-  useEffect(() => {
-    fetch("/api/advertisements")
-      .then((response) => response.json())
-      .then((data) => setAdvertisementData(data[0]));
-  }, []);
+import imageUrlBuilder from "@sanity/image-url";
 
+const AdvertisementTop = () => {
+  const [AdvertisementImage, setAdvertisementImage] = useState();
+  useEffect(() => {
+    const imgBuilder = imageUrlBuilder({
+      projectId: "cqnczxva",
+      dataset: "production",
+    });
+    fetch("/api/topAdvertisement")
+      .then((response) => response.json())
+      .then((data) =>
+        setAdvertisementImage(imgBuilder.image(data[0].mainImage))
+      );
+  }, []);
   return (
     <div className="h-fill bg-[#0398b6] mt-[60px]">
       <div className="flex justify-center align-center py-4">
         <a href="https://www.mimirquiz.com/" target="_blank">
-          {/* <img src={AdvertisementData.homePageTopAd} alt="" /> */}
+          <img src={AdvertisementImage} alt="" />
         </a>
       </div>
     </div>
--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="h-screen w-screen bg-no-repeat bg-cover bg-[url('https://res.cloudinary.com/nxtgem-io/image/upload/v1640675180/ezgif.com-gif-maker_2_txkczt.gif')]"></div>
+    <div className="h-screen w-screen bg-no-repeat bg-contain bg-black md:bg-cover  bg-[url('https://res.cloudinary.com/nxtgem-io/image/upload/v1640675180/ezgif.com-gif-maker_2_txkczt.gif')]"></div>
   );
 };
 
