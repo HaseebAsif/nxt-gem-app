6cdf839dad810928c73a19db496e6d626c682589
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-03 21:27:32 +0500

    Loader updated

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="h-screen w-screen bg-no-repeat bg-contain bg-black md:bg-cover  bg-[url('https://res.cloudinary.com/nxtgem-io/image/upload/v1640675180/ezgif.com-gif-maker_2_txkczt.gif')]"></div>
+    <div className="flex items-center justify-center h-screen w-screen bg-black">
+      <img
+        className="bg-contain bg-black bg-no-repeat md:bg-cover md:w-screen md:h-screen"
+        src="/loader.gif"
+      />
+    </div>
   );
 };
 
