c581f3d113cf86fb1b2d00f7d4bb41e41a895971
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-04 13:01:49 +0500

    Our partners link added

--- src/components/ourPartners/index.jsx
+++ src/components/ourPartners/index.jsx
@@ -9,19 +9,25 @@ const OurPartners = () => {
     const url = `https://cqnczxva.api.sanity.io/v1/data/query/production?query=${query}`;
 
     const result = await fetch(url).then((res) => res.json());
-    setPartnersData(result.result);
+    setPartnersData(
+      result.result.sort((a, b) => parseFloat(a.order) - parseFloat(b.order))
+    );
   }, []);
   return (
     <div className="py-12 mt-24 animate-fadeIn sm:mt-0">
       <SectionHeader heading="Our Partners" value="5" />
-      <div className="flex items-center justify-center flex-col sm:flex-row h-[50vh]">
-        {PartnersData.map(({ mainImage, imgAlt }) => {
+      <div className="flex items-center justify-center flex-col flex-wrap w-full sm:flex-row min-h-[50vh] h-full">
+        {PartnersData.slice(0, 6).map(({ mainImage, imgAlt, link }) => {
           const imgBuilder = imageUrlBuilder({
             projectId: "cqnczxva",
             dataset: "production",
           });
           const imgSrc = imgBuilder.image(mainImage);
-          return <img className="w-10/12 sm:w-1/4" src={imgSrc} alt={imgAlt} />;
+          return (
+            <a href={link} target="_blank" className="w-[300px] sm:w-[450px]">
+              <img src={imgSrc} alt={imgAlt} />
+            </a>
+          );
         })}
       </div>
     </div>
