a8c34997587b32ef9dda7ab85092e9866f5620cc
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-04 23:40:13 +0500

    Loader error resolved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
   });
   return (
     <>
-      {!isLoading ? (
+      {isLoading ? (
         <Layout>
           <Component {...pageProps} />
         </Layout>
