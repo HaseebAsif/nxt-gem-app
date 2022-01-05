fc8f85bbb3db6ba1b7db3aa3e3e293677ecb6fc8
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-01-05 15:06:11 +0500

    animation added

--- src/components/ourTeam/index.jsx
+++ src/components/ourTeam/index.jsx
@@ -2,6 +2,7 @@ import imageUrlBuilder from "@sanity/image-url";
 
 import SectionHeader from "components/sectionHeading";
 import React, { useEffect, useState } from "react";
+import Fade from "react-reveal/Fade";
 
 import OurTeamName from "./ourTeamName";
 
@@ -18,89 +19,91 @@ const OurTeam = () => {
   }, []);
 
   return (
-    <div className="animate-fadeIn">
-      <SectionHeader heading="Our Team" value="4" />
-      <div className="grid grid-cols-1 place-items-center mx-28 sm:mx-48 lg:grid-cols-3 sm:grid-cols-2">
-        {OurTeamData.map(
-          ({
-            id,
-            name,
-            mainImage,
-            post,
-            facebook,
-            mainImageShadow,
-            linkedin,
-            telegram,
-            twitter,
-          }) => {
-            const imgBuilder = imageUrlBuilder({
-              projectId: "cqnczxva",
-              dataset: "production",
-            });
-            const imageSrc = imgBuilder.image(mainImage);
-            const imgHoverSrc = imgBuilder.image(mainImageShadow);
-            return (
-              <div key={id} className="mt-12">
-                <div className="h-[180px]">
-                  <img
-                    src={imageSrc}
-                    alt=""
-                    className="max-w-[180px] drop-shadow-2xl"
-                  />
-                  <img
-                    src={imgHoverSrc}
-                    alt=""
-                    className="max-w-[180px] relative bottom-[180px] hover:hidden drop-shadow-xl"
-                  />
+    <Fade right>
+      <div className="animate-fadeIn">
+        <SectionHeader heading="Our Team" value="4" />
+        <div className="grid grid-cols-1 place-items-center mx-28 sm:mx-48 lg:grid-cols-3 sm:grid-cols-2">
+          {OurTeamData.map(
+            ({
+              id,
+              name,
+              mainImage,
+              post,
+              facebook,
+              mainImageShadow,
+              linkedin,
+              telegram,
+              twitter,
+            }) => {
+              const imgBuilder = imageUrlBuilder({
+                projectId: "cqnczxva",
+                dataset: "production",
+              });
+              const imageSrc = imgBuilder.image(mainImage);
+              const imgHoverSrc = imgBuilder.image(mainImageShadow);
+              return (
+                <div key={id} className="mt-12">
+                  <div className="h-[180px]">
+                    <img
+                      src={imageSrc}
+                      alt=""
+                      className="max-w-[180px] drop-shadow-2xl"
+                    />
+                    <img
+                      src={imgHoverSrc}
+                      alt=""
+                      className="max-w-[180px] relative bottom-[180px] hover:hidden drop-shadow-xl"
+                    />
+                  </div>
+                  <OurTeamName name={name} />
+                  <div className="text-white text-center text-lg mb-2">
+                    {post}
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <a href={facebook} className="mx-1" target="_blank">
+                      <img
+                        src={
+                          "https://res.cloudinary.com/nxtgem-io/image/upload/v1640600818/TEAM-Facebook-Button_dgyvum.png"
+                        }
+                        alt={facebook}
+                        className="w-[30px]"
+                      />
+                    </a>
+                    <a href={telegram} className="mx-1" target="_blank">
+                      <img
+                        src={
+                          "https://res.cloudinary.com/nxtgem-io/image/upload/v1640600819/TEAM-Telegram-Button_dqem53.png"
+                        }
+                        alt={telegram}
+                        className="w-[30px]"
+                      />
+                    </a>
+                    <a href={linkedin} className="mx-1" target="_blank">
+                      <img
+                        src={
+                          "https://res.cloudinary.com/nxtgem-io/image/upload/v1640600819/TEAM-LinkedIn-Button_uj2gdj.png"
+                        }
+                        alt={linkedin}
+                        className="w-[30px]"
+                      />
+                    </a>
+                    <a href={twitter} className="mx-1" target="_blank">
+                      <img
+                        src={
+                          "https://res.cloudinary.com/nxtgem-io/image/upload/v1640600819/TEAM-Twitter-Button_hfn0qx.png"
+                        }
+                        alt={twitter}
+                        className="w-[30px]"
+                      />
+                    </a>
+                  </div>
                 </div>
-                <OurTeamName name={name} />
-                <div className="text-white text-center text-lg mb-2">
-                  {post}
-                </div>
-                <div className="flex items-center justify-center">
-                  <a href={facebook} className="mx-1" target="_blank">
-                    <img
-                      src={
-                        "https://res.cloudinary.com/nxtgem-io/image/upload/v1640600818/TEAM-Facebook-Button_dgyvum.png"
-                      }
-                      alt={facebook}
-                      className="w-[30px]"
-                    />
-                  </a>
-                  <a href={telegram} className="mx-1" target="_blank">
-                    <img
-                      src={
-                        "https://res.cloudinary.com/nxtgem-io/image/upload/v1640600819/TEAM-Telegram-Button_dqem53.png"
-                      }
-                      alt={telegram}
-                      className="w-[30px]"
-                    />
-                  </a>
-                  <a href={linkedin} className="mx-1" target="_blank">
-                    <img
-                      src={
-                        "https://res.cloudinary.com/nxtgem-io/image/upload/v1640600819/TEAM-LinkedIn-Button_uj2gdj.png"
-                      }
-                      alt={linkedin}
-                      className="w-[30px]"
-                    />
-                  </a>
-                  <a href={twitter} className="mx-1" target="_blank">
-                    <img
-                      src={
-                        "https://res.cloudinary.com/nxtgem-io/image/upload/v1640600819/TEAM-Twitter-Button_hfn0qx.png"
-                      }
-                      alt={twitter}
-                      className="w-[30px]"
-                    />
-                  </a>
-                </div>
-              </div>
-            );
-          }
-        )}
+              );
+            }
+          )}
+        </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
--- src/components/topViews/index.jsx
+++ src/components/topViews/index.jsx
@@ -2,6 +2,8 @@ import SectionHeader from "components/sectionHeading";
 import SingleLargeVideoViews from "components/singleLargeVideoViews";
 import SingleSmallVideoViews from "components/singleSmallVideoViews";
 import { useEffect, useState } from "react";
+import Fade from "react-reveal/Fade";
+
 const TopViews = () => {
   const [TopViewsData, setTopViewsData] = useState([]);
   useEffect(async () => {
@@ -12,31 +14,33 @@ const TopViews = () => {
     setTopViewsData(result.result);
   }, []);
   return (
-    <div className="mb-24 sm:mb-32 animate-fadeIn">
-      <div className="mt-8">
-        <SectionHeader heading="Top Views" value="2" />
-      </div>
-      <div className="flex items-center justify-center flex-col lg:flex-row lg:mx-12 xl:mx-48">
-        {TopViewsData.slice(0, 1).map(({ link }) => (
-          <SingleLargeVideoViews videoUrl={link} />
-        ))}
+    <Fade left>
+      <div className="mb-24 sm:mb-32 animate-fadeIn">
+        <div className="mt-8">
+          <SectionHeader heading="Top Views" value="2" />
+        </div>
+        <div className="flex items-center justify-center flex-col lg:flex-row lg:mx-12 xl:mx-48">
+          {TopViewsData.slice(0, 1).map(({ link }) => (
+            <SingleLargeVideoViews videoUrl={link} />
+          ))}
 
-        <div className="lg:ml-[80px]">
-          <div className="flex justify-center items-center mt-5 lg:mt-0">
-            <img
-              src="https://res.cloudinary.com/nxtgem-io/image/upload/v1640600233/Sponsered_rbijdc.png"
-              className="object-contain"
-              alt=""
-            />
-          </div>
-          <div className="flex flex-row flex-wrap justify-center lg:flex-col">
-            {TopViewsData.slice(1, 4).map(({ link }) => (
-              <SingleSmallVideoViews videoUrl={link} />
-            ))}
+          <div className="lg:ml-[80px]">
+            <div className="flex justify-center items-center mt-5 lg:mt-0">
+              <img
+                src="https://res.cloudinary.com/nxtgem-io/image/upload/v1640600233/Sponsered_rbijdc.png"
+                className="object-contain"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-row flex-wrap justify-center lg:flex-col">
+              {TopViewsData.slice(1, 4).map(({ link }) => (
+                <SingleSmallVideoViews videoUrl={link} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
