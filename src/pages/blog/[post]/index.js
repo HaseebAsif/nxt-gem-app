98268376929653c8d30488f4e58c60a97e14ce8a
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-15 23:09:48 +0500

    feat: Full screen blog page slider added

--- src/pages/blog/[post]/index.js
+++ src/pages/blog/[post]/index.js
@@ -1,9 +1,35 @@
+import useSlider from "hooks/useSlider";
+import React, { useRef, useState, useEffect } from "react";
 import imageUrlBuilder from "@sanity/image-url";
-import { useState, useEffect } from "react";
 import SanityBlockContent from "@sanity/block-content-to-react";
 
-export const Post = ({ image, body, title }) => {
+import styles from "./post.module.css";
+
+const SinglePost = ({ image, body, title }) => {
   const [imageUrl, setImageUrl] = useState("");
+  const slideImage = useRef(null);
+  const slideText = useRef(null);
+
+  const images = [
+    {
+      src: imageUrl,
+      text: body && <SanityBlockContent blocks={body} />,
+    },
+    {
+      src: imageUrl,
+      text: "Consectetur adipisicing elit cillum dolore eu fugiat nulla",
+    },
+    {
+      src: imageUrl,
+      text: "Asperiores ex animi explicabo cillum dolore eu fugiat nulla",
+    },
+  ];
+
+  const { goToPreviousSlide, goToNextSlide } = useSlider(
+    slideImage,
+    slideText,
+    images
+  );
 
   useEffect(() => {
     const imgBuilder = imageUrlBuilder({
@@ -13,22 +39,26 @@ export const Post = ({ image, body, title }) => {
 
     setImageUrl(imgBuilder.image(image));
   }, [image]);
-
   return (
-    <div className="px-2 sm:px-28 mt-24">
-      <div>
-        <h1 className="font-bold text-3xl">{title}</h1>
-        {imageUrl && <img src={imageUrl} />}
-
-        <div className="body__singlepost">
-          {body && <SanityBlockContent blocks={body} />}
+    <div className={styles.slider} ref={slideImage}>
+      <div className={styles.slider__content}>
+        <button onClick={goToPreviousSlide} className={styles.slider__btn_left}>
+          <i className="fas fa-angle-left"></i>
+        </button>
+        <div className={styles.slider__feature}>
+          <h1 className={styles.feature__title}>Dreaming</h1>
+          <p ref={slideText} className={styles.feature__text}></p>
+          <button className={styles.feature__btn}>Get started</button>
         </div>
+        <button onClick={goToNextSlide} className={styles.slider__btn_right}>
+          <i className="fas fa-angle-right"></i>
+        </button>
       </div>
     </div>
   );
 };
 
-Post.getInitialProps = async (pageContext) => {
+SinglePost.getInitialProps = async (pageContext) => {
   const pageSlug = pageContext.query.post;
 
   if (!pageSlug) {
@@ -58,4 +88,4 @@ Post.getInitialProps = async (pageContext) => {
   }
 };
 
-export default Post;
+export default SinglePost;
