bb1fef9e79bae43975bfb7d035758f51e715665d
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-16 15:21:51 +0500

    feat: slider update

--- src/pages/blog/[post]/index.js
+++ src/pages/blog/[post]/index.js
@@ -1,63 +1,38 @@
-import useSlider from "hooks/useSlider";
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
 import imageUrlBuilder from "@sanity/image-url";
 import SanityBlockContent from "@sanity/block-content-to-react";
 
-import styles from "./post.module.css";
+import ImageSlider from "components/slider";
 
+const images = [
+  "https://source.unsplash.com/WLUHO9A_xik/1600x900",
+  "https://source.unsplash.com/WLUHO9A_xik/1600x900",
+  "https://source.unsplash.com/wQLAGv4_OYs/1600x900",
+  "https://source.unsplash.com/WLUHO9A_xik/1600x900",
+  "https://source.unsplash.com/WLUHO9A_xik/1600x900",
+  "https://source.unsplash.com/WLUHO9A_xik/1600x900",
+];
 const SinglePost = ({ image, body, title }) => {
-  const [imageUrl, setImageUrl] = useState("");
-  const slideImage = useRef(null);
-  const slideText = useRef(null);
-
-  const images = [
-    {
-      src: imageUrl,
-      text: body && <SanityBlockContent blocks={body} />,
-    },
-    {
-      src: imageUrl,
-      text: "Consectetur adipisicing elit cillum dolore eu fugiat nulla",
-    },
-    {
-      src: imageUrl,
-      text: "Asperiores ex animi explicabo cillum dolore eu fugiat nulla",
-    },
-  ];
-
-  const { goToPreviousSlide, goToNextSlide } = useSlider(
-    slideImage,
-    slideText,
-    images
-  );
-
-  useEffect(() => {
-    const imgBuilder = imageUrlBuilder({
-      projectId: "cqnczxva",
-      dataset: "production",
-    });
-
-    setImageUrl(imgBuilder.image(image));
-  }, [image]);
   return (
-    <div className={styles.slider} ref={slideImage}>
-      <div className={styles.slider__content}>
-        <button onClick={goToPreviousSlide} className={styles.slider__btn_left}>
-          <i className="fas fa-angle-left"></i>
-        </button>
-        <div className={styles.slider__feature}>
-          <h1 className={styles.feature__title}>Dreaming</h1>
-          <p ref={slideText} className={styles.feature__text}></p>
-          <button className={styles.feature__btn}>Get started</button>
-        </div>
-        <button onClick={goToNextSlide} className={styles.slider__btn_right}>
-          <i className="fas fa-angle-right"></i>
-        </button>
+    <>
+      <div>
+        <ImageSlider images={images}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              color: "#fff",
+            }}
+          >
+            <h1>React.js Image Slider</h1>
+            <p>Nulla vitae elit libero, a pharetra augue.</p>
+          </div>
+        </ImageSlider>
       </div>
-    </div>
+    </>
   );
 };
-
 SinglePost.getInitialProps = async (pageContext) => {
   const pageSlug = pageContext.query.post;
 
