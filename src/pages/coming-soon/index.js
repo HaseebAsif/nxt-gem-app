d4ad938f5669f39e992750dfcddd08728f836de9
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-01-18 22:05:44 +0500

    padding added in coming soon page

--- src/pages/coming-soon/index.js
+++ src/pages/coming-soon/index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const ComingSoon = () => {
   return (
-    <div className="bg-[url('https://res.cloudinary.com/nxtgem-io/image/upload/c_scale,w_680/v1640600879/background_app_klirup.png')] h-fit bg-no-repeat bg-cover p-8 sm:p-24 lg:p-48 lg:pt-24">
+    <div className="bg-[url('https://res.cloudinary.com/nxtgem-io/image/upload/c_scale,w_680/v1640600879/background_app_klirup.png')] h-fit bg-no-repeat bg-cover p-8 sm:p-24 lg:p-48 lg:pt-32">
       <SectionHeader heading="Coming Soon" value="6" />
       <div className="mt-5">
         <div className="grid grid-cols-1 sm:grid-cols-3 text-white">
