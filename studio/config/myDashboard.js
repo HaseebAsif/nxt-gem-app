bd911914706fea0d05fd97818fdaa16a9272404b
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-01-25 15:18:41 +0500

    feat: google-analytics added

--- studio/config/myDashboard.js
+++ studio/config/myDashboard.js
@@ -46,5 +46,32 @@ export default {
         },
       },
     },
+    {
+      name: "project-info",
+      options: {
+        data: [
+          {
+            title: "GitHub repo",
+            value: "https://github.com/haseebasif/nxt-gem-app",
+            category: "Code",
+          },
+          {
+            title: "Frontend",
+            value: "https://nxtgem.io",
+            category: "apps",
+          },
+        ],
+      },
+    },
+    { name: "project-users", layout: { height: "auto" } },
+    {
+      name: "document-list",
+      options: {
+        title: "Recent blog posts",
+        order: "_createdAt desc",
+        types: ["post"],
+      },
+      layout: { width: "medium" },
+    },
   ],
 };
