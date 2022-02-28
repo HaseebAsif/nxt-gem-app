61d88c28ff35bb600a973992ccaaaab95b8477ae
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-03-01 00:01:56 +0500

    box designed

--- src/components/topStoriesMainSlider/index.jsx
+++ src/components/topStoriesMainSlider/index.jsx
@@ -4,9 +4,11 @@ const TopStoriesMainSlider = ({ image }) => {
   return (
     <div className="h-[74vh] bg-red-800">
       <div
-        className="object-cover h-full bg-no-repeat bg-cover"
+        className="object-cover h-full bg-no-repeat bg-cover flex justify-end"
         style={{ backgroundImage: `url(${image})` }}
-      />
+      >
+        <div className="h-80 w-72 bg-blue-700 mr-20"></div>
+      </div>
     </div>
   );
 };
